6cdf36c32e7ac9babbe27e5f42f1941dd15fdede
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-26 19:11:15 +0500

    Button component removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Button>Click Me</Button>
-      </main>
     </>
   );
 }
